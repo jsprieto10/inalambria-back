11dacadd37b9bb20d46c4067be009b6a9e4bee1c
jsprieto10 <js.prieto10@uniandes.edu.co>
2020-08-17 17:36:27 -0500

    cambios

--- configurePassport.js
+++ configurePassport.js
@@ -74,7 +74,7 @@ passport.serializeUser(function (user, cb) {
         resave: false,
         saveUninitialized: false,
         cookie : {
-          sameSite: 'none', // THIS is the config you are looing for.
+          sameSite: 'strict', // THIS is the config you are looing for.
           secure: true,
         }
       })
